fa9dc180f26a280404ef8404b554ed1ca801be53
mvargas44 <mvargas44@cuc.edu.co>
2024-09-24 13:04:18 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miLogo from "../src/assets/img/png-transparent-cat-kitty-creative-cat-cat.png"
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <img src={miLogo} alt="Logo React" />
+      <h1>Hola!</h1>
+      <p>Este es mi taller de React...</p>
+    </div>
+    </>
+  )
+}
+
+export default App
